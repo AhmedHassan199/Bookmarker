d1ad82f96f0de15ad74915e4a1aaef4c5d30374a
Ahmed Mohamed <amohamed@uittai.com>
2024-05-28 00:33:10 +0300

    search

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var form = document.getElementById('bookmarkForm');
 var siteUrl = document.getElementById('siteUrl');
 var siteName = document.getElementById('siteName');
+var updateButton = document.getElementById('updateButton');
 var books = JSON.parse(localStorage.getItem('data')) ?? [];
 var bookmarkList = document.getElementById('bookmarkList');
-display();
+var submitButton = document.getElementById('submitButton');
+var searchInput = document.getElementById('searchInput');
+
+display(books);
 form.addEventListener('submit', function(event) {
     event.preventDefault(); 
+    console.log("Dsa");
     addBook();
     clearForm()
 });
@@ -17,15 +22,15 @@ form.addEventListener('submit', function(event) {
     }
     books.push(bookData);
     localStorage.setItem('data' , JSON.stringify( books))
-    display();
+    display(books);
 }
 function clearForm(){
     siteName.value = null ;
     siteUrl.value = null ;
 }
-function display(){
+function display(data){
     bookmarkList.innerHTML = '';
-    books.forEach(function(bookmark, index) {
+    data.forEach(function(bookmark, index) {
         var row = bookmarkList.insertRow(index);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
@@ -33,15 +38,36 @@ function display(){
         var cell4 = row.insertCell(3);
         cell1.innerHTML = index + 1;
         cell2.innerHTML = bookmark.siteName;
-        cell3.innerHTML = `<a href="${bookmark.siteUrl}" target="_blank" class="btn btn-success">Visit</a>`;
-        cell4.innerHTML = `<button class="btn btn-danger" onclick="deleteBookmark(${index})">Delete</button>`;
+        cell3.innerHTML = `<a href="${bookmark.siteUrl}" target="_blank" class="btn btn-success">Visit</a>  `;
+        cell4.innerHTML = `<button class="btn btn-danger" onclick="deleteBookmark(${index})">Delete</button> <button class="btn btn-success" type = 'button' onclick="editBookmark(${index})">Edit</button>`;
     });
 }
 function deleteBookmark(index){
   
     books.splice(index, 1);
     localStorage.setItem('data' , JSON.stringify( books))
-    display()
+    display(books)
+}
+var updatedIndex ;
+function editBookmark(index){
+    siteName.value = books[index]['siteName'];
+  siteUrl.value = books[index]['siteUrl'];
+  updatedIndex =  index ;
+  updateButton.classList.add("d-block");
+  submitButton.classList.add("d-none");
+
+}
+updateButton.addEventListener('click', function() {
+  updateBookmark(updatedIndex)
+});
+function updateBookmark(index){
+  books[index]['siteName'] = siteName.value ;
+  books[index]['siteUrl'] =  siteUrl.value ;
+  localStorage.setItem('data' , JSON.stringify( books))
+  display(books)
+  submitButton.classList.remove("d-none");
+  updateButton.classList.remove("d-block");
+
 }
 document.getElementById('siteUrl').addEventListener('input', function () {
     const urlInput = this;
@@ -59,3 +85,18 @@ document.getElementById('siteUrl').addEventListener('input', function () {
       urlFeedback.style.display = 'none';
     }
   });
+  function search(word){
+    var searchedData = [];
+    for (let i = 0; i < books.length; i++) {
+
+      if(books[i]['siteName'].trim().toLowerCase().includes(word.toLowerCase())   ){
+        searchedData.push(books[i]);
+      }
+     
+    }
+   
+    display( searchedData);
+  }
+  searchInput.addEventListener('input', function(event) {
+search(this.value);
+});
